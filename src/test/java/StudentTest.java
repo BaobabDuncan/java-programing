7d593e9f1239fa4477a99db8e96d2f1eae2c277e
BaobabDuncan <baobab.duncan@gmail.com>
2017-02-01 22:37:48 +0900

    lesson 01

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,4 +4,16 @@ import junit.framework.TestCase;
  * Created by Administrator on 2017-02-01.
  */
 public class StudentTest extends TestCase {
+    public void testCreate(){
+
+        final String firstStudentName = "Jane Doe";
+        Student firstStudent = new Student(firstStudentName);
+        assertEquals(firstStudentName, firstStudent.getName());
+
+
+        final String secondStudentName = "Joe Blow";
+        Student secondStudent = new Student(secondStudentName);
+        assertEquals(secondStudentName, secondStudent.getName());
+
+    }
 }
